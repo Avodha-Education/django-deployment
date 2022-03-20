8f38d69c4e288344a4f34d4174a6c1764c4c5e18
SabarinathK <sabarinathodc@gmail.com>
2022-03-20 13:45:49 +0530

    'upload_files'

---
+++ ProjectApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from ProjectApp import views
+
+urlpatterns = [
+    path('',views.home,name='name'),
+    path('predict/',views.prediction,name='prediction')
+    
+
+]
